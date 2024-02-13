eb50829d3009056ca0398ce1a35227f4dec58edc
suhail-habid <159782732+suhail-habid@users.noreply.github.com>
2024-02-13 12:53:37 +0200

    Update settingOptions.ts

--- src/store/appSettings/settingOptions.ts
+++ src/store/appSettings/settingOptions.ts
@@ -1,8 +1,8 @@
 const appRegionOptions = [
-    { label: 'US East (N. Virginia)', value: 'us-east-1' },
+    { label: 'US East (N. Virginia)', value: 'us-east-1', disabled: true },
     { label: 'US East (Ohio)', value: 'us-east-2', disabled: true },
     { label: 'US East (N. Virginia)', value: 'us-west-1', disabled: true },
-    { label: 'US West (Oregon)', value: 'us-west-2', disabled: true },
+    { label: 'US West (Oregon)', value: 'us-west-2' },
 ];
 
 const apiTimings = [
